6760405645587f869f84445ba86f25e79e5cd718
SBIvanov22 <sbivanov22@codingburgas.bg>
2024-04-21 19:01:23 +0300

    Add menu and changes

--- src/main/menu.cpp
+++ src/main/menu.cpp
@@ -16,6 +16,45 @@ void mainMenu() {
 
 
     while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        ClearBackground(WHITE);
+
+        DrawTexture(logo, (screenWidth / 2 - logo.width / 2), 120, WHITE);
+
+
+        DrawText("Anthanta HQ", screenWidth / 2.1 - MeasureText("History Quiz", 30) + 40, 70, 55, BLACK);
+
+
+        // Draw the "Play" option
+        Rectangle playButton = { block1X, blockY, 160, 80 };
+        DrawRectangleRec(playButton, SKYBLUE);
+        DrawText("START", block1X + 30, blockY + 30, 30, WHITE);
+
+        // Draw the "Exit" option
+        Rectangle exitButton = { block2X, blockY , 160, 80 };
+        DrawRectangleRec(exitButton, SKYBLUE);
+        DrawText("EXIT", block2X + 40, blockY + 30, 30, WHITE);
+
+        // Check if the mouse is over the "Start" button
+        if (CheckCollisionPointRec(GetMousePosition(), playButton)) {
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+               
+            }
+        }
+
+        // Check if the mouse is over the "Exit" button
+        if (CheckCollisionPointRec(GetMousePosition(), exitButton)) {
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) || IsKeyPressed(KEY_ESCAPE)) {
+
+                UnloadTexture(logo);
+
+                CloseWindow();
+            }
+        }
+
+        EndDrawing();
     }
+
 
 }
